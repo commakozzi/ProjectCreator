a431dc23e3016e37cbdf93ab80820d8e9383b9af
commakozzi <commakozzi1@gmail.com>
2016-06-08 23:29:28 -0400

    Add project files.

---
+++ ProjectCreator/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace ProjectCreator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Define Variables:
+        string mProjectNumber;
+        string mProjectName;
+        string mPath;
+
+        private void createProject_button_Click(object sender, RoutedEventArgs e)
+        {
+            mProjectNumber = projectName_textbox.Text;
+            mProjectName = projectNumber_textbox.Text;
+            mPath = @"c:\" + mProjectNumber + "." + mProjectName;
+
+            try
+            {
+                if (Directory.Exists(mPath))
+                {
+                    information_label.Content = "That path already exists.";
+                    return;
+                }
+
+                // Try to create directory:
+                DirectoryInfo di = Directory.CreateDirectory(mPath);
+                information_label.Content = "The directory was created successfully";
+            }
+            catch (Exception err)
+            {
+                information_label.Content = "The directory creation failed.";
+            }
+            finally {}
+            
+
+        }
+    }
+}
